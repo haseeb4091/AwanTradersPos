21843896257b75ce521d830c9b44d98780174688
Abdul haseeb <jasialnew1@gmail.com>
2021-08-31 16:23:43 +0500

    first commit

--- AwanTraders/Form1.Designer.cs
+++ AwanTraders/Form1.Designer.cs
@@ -38,6 +38,7 @@ namespace AwanTraders
             this.ClientSize = new System.Drawing.Size(920, 512);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- AwanTraders/Form1.cs
+++ AwanTraders/Form1.cs
@@ -17,5 +17,9 @@ namespace AwanTraders
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            abc
+        }
     }
 }
